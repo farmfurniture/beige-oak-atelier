ee061ed3296652b1a6fec4ff91c3c32055527e5f
NapsterMayank <napster.mayank16@gmail.com>
2025-10-11 14:51:45 +0530

    product details

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CartProvider } from "@/context/CartContext";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>{children}</CartProvider>
+    </QueryClientProvider>
   );
 }
